ddb5cb518fe32e5f0b76e3cfe7ac30efb0922849
EvilPug <3608690@gmail.com>
2022-09-23 19:18:54 +0300

    Initial commit

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public float speed = 5f;
+
+    public Rigidbody2D rb;
+    public Animator anim;
+
+    Vector2 movement;
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime;
+        movement.y = Input.GetAxisRaw("Vertical") * speed * Time.deltaTime;
+
+        anim.SetFloat("Horizontal", movement.x);
+        anim.SetFloat("Vertical", movement.y);
+        anim.SetFloat("Speed", movement.sqrMagnitude);
+    }
+
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+    }
+}
---
+++ Assets/Scripts/PlayerInteraction.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteraction : MonoBehaviour
+{
+    public Animator fridge_anim;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("interactable"))
+        {
+            fridge_anim.SetBool("is_opened", true);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("interactable"))
+        {
+            fridge_anim.SetBool("is_opened", false);
+        }
+    }
+
+
+}
+
